f078a236eff9ab35eea74c2ee573b9ed89c02ab4
Katherine Scott <kasc1625@gmail.com>
2023-06-28 18:24:27 -0700

    Finishing the JS	modified:   index.js

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+const billInput = document.getElementById('billTotalInput');
 
-calculateBill(){
+const tipInput = document.getElementById('tipInput');
 
-}
+const numberOfPeopleDiv = document.getElementById('numberOfPeople');
 
-decreasePeople(){
+const perPersonTotalDiv = document.getElementById('perPersonTotal');
 
-}
+let numberOfPeople = Number(numberOfPeopleDiv.innerText);
 
-increasePeople(){
-    
-}
+const calculateBill = () => {
+  const bill = Number(billInput.value);
+  const tipPercentage = Number(tipInput.value) / 100;
+  const tipAmount = bill * tipPercentage;
+  const total = tipAmount + bill;
+  const perPersonTotal = total / numberOfPeople;
+
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`;
+};
+
+const decreasePeople = () => {
+  if (numberOfPeople <= 1) {
+    return;
+  }
+  numberOfPeople -= 1
+
+  numberOfPeopleDiv.innerText =numberOfPeople
+};
+const increasePeople = () => {
+  numberOfPeople += 1;
+
+  numberOfPeopleDiv.innerText = numberOfPeople;
+
+  calculateBill();
+};
